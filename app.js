3f15992e99e9f1aa7704e1822ed1872faba250ec
Uka Dashdavaa <108192613+UkaDash@users.noreply.github.com>
2024-03-08 11:48:30 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 8080;
 
 // Serve the image file instead of the text
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/image.jpg'); // Replace 'path_to_your_image.jpg' with the actual path to your image file
+    res.sendFile(__dirname + '/cloud.jpg'); // Replace 'path_to_your_image.jpg' with the actual path to your image file
 });
 
 app.listen(port);
