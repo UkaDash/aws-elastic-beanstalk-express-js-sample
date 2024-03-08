bad9b2971264b5e77c981eabe90129ac0610daf5
Uka Dashdavaa <108192613+UkaDash@users.noreply.github.com>
2024-03-08 11:40:37 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('CumulusCodex'));
+// Serve the image file instead of the text
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/image.jpg'); // Replace 'path_to_your_image.jpg' with the actual path to your image file
+});
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
